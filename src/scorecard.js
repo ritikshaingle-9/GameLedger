ab17758b413066b8ee28ad83558279124e2015cd
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-02-14 00:09:50 +0530

    logic added

--- src/scorecard.js
+++ src/scorecard.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 import stadium from './images/stadium-3.jpg'
 import Card from './card'
 
 export default function Scorecard(){     
   const [scoreA,setScoreA]= useState(0);
   const [scoreB,setScoreB]= useState(0);
+  const [winner,setWinner]= useState(null); 
+
+  const MAX_SCORE=5;
   useEffect(() => {
-    if(scoreA<10 && scoreB<10){
+    if(scoreA<MAX_SCORE && scoreB<MAX_SCORE){
       return;
     }
 
-    if(scoreA==10){
-      alert('team a');
+    if(scoreA==MAX_SCORE){
+      setWinner("Team A");
+      toast.success('Team A wins!!');
     }
 
-    if(scoreB==10){
-      alert('team b');
+    if(scoreB==MAX_SCORE){
+      setWinner("Team B");
+      toast.success('Team B wins!!');
     }
   },[scoreA,scoreB]);
   
@@ -29,19 +35,28 @@ export default function Scorecard(){
     <div className='flex flex-col md:flex-row justify-around mt-2 md:mt-6'>
     <Card score={scoreA} teamName='Team A' 
     increasescore={()=>setScoreA(scoreA +1)}
-    decreasescore={()=>setScoreA(scoreA -1)}/>
+    decreasescore={()=>setScoreA(scoreA -1)}
+    winner={winner}/>
 
    <Card score={scoreB} teamName='Team B' 
     increasescore={()=>setScoreB(scoreB +1)}
-    decreasescore={()=>setScoreB(scoreB -1)}/>
+    decreasescore={()=>setScoreB(scoreB -1)}
+    winner={winner}/>
    </div>
+   
+   {winner?(
+   <p className='text-center text-2xl text-white my-7'>Winner Team is <b className='underline decoration-wavy mx-4 bg-red-500 p-3'>"{winner}🏆🏆"</b>.Click reset to start again</p>):null
+   }
+
     <div className='flex justify-center'>
-      <button className='text-3xl py-3 px-10 text-center rounded-xl bg-[rgba(173,216,230,0.8)] '
+      <button className='text-2xl md:text-3xl py-3 px-7 md:px-10 text-center rounded-xl bg-[rgba(173,216,230,0.8)] '
       onClick={()=>{
           setScoreA(0);
           setScoreB(0);
+          setWinner(null);
       }}>Reset</button>
     </div>
+    <Toaster/>
     </div>
   )
 }
